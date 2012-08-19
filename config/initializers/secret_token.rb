dd295957effa77e74626fc87f2861972ebeee16f
Konstantin Lysenko <konstantin@atalanta-systems.com>
2012-08-19 17:25:14 +0300

    Init RoR application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moodboard::Application.config.secret_token = '05a149edcfe9673da5471f9d8f5a6d4dd0b725f13327b1caf76e15e8fa6c1c8827904502653741fc19dd8d11547934cef2fe6e99fc2597b14cdefd5d8016ee09'
