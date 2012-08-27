449a87fd2929b53498d4af1efa3160cbb385e67f
Konstantin Lysenko <gshaud@gmail.com>
2012-08-28 01:05:36 +0300

    Delete moodboard controllers in favour of moods controller

--- app/controllers/moodboard_controller.rb
+++
@@ -1,7 +0,0 @@
-class MoodboardController < ApplicationController
-
-  def index
-    @persons = ['SNS','HPMNS','KL','SB','MP','AB','YT','SA','JB']
-  end
-  
-end
--- test/unit/helpers/moodboard_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MoodboardHelperTest < ActionView::TestCase
-end
