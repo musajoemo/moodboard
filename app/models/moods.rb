88375daee2650c6cf81c0da39605dee4f9569f8c
Konstantin Lysenko <gshaud@gmail.com>
2012-09-12 22:08:12 +0300

    Add non-null validates to moods model

--- app/models/moods.rb
+++ app/models/moods.rb
@@ -1,3 +1,6 @@
 class Moods < ActiveRecord::Base
   attr_accessible :date, :mood, :person_id
+  validates :mood, :presence => true
+  validates :date :presence => true
+  validates :person_id, :presence => true
 end
